29bf68b2461717ae3f913061cd4837a3d11c6a3b
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-08-03 16:07:37 +0000

    Added ratings as a parameter for books

--- code.js
+++ code.js
@@ -1,17 +1,19 @@
 const myLibrary = [];
 
-// Here, author, title = String, numberOfPages = int and hasBeenRead = boolean 
-function Book(author, title, numberOfPages, hasBeenRead) {
+// Here, author, title = String, numberOfPages = int and hasBeenRead = boolean , 
+// rating should be a number between 1 and 10
+function Book(author, title, numberOfPages, hasBeenRead, rating) {
     this.author = author;
     this.title = title;
     this.numberOfPages = numberOfPages;
     this.hasBeenRead = hasBeenRead;
+    this.rating = rating;
     this.id = crypto.randomUUID();
 }
 
 // Add the book to the library
-function addBookToLibrary(author, title, numberOfPages, hasBeenRead) {
-    newBook = new Book(author, title, numberOfPages, hasBeenRead);
+function addBookToLibrary(author, title, numberOfPages, hasBeenRead, rating) {
+    newBook = new Book(author, title, numberOfPages, hasBeenRead, rating);
     myLibrary.push(newBook);
 }
 
