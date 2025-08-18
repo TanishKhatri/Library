1cdb14645352d6232592120951941bb91625ed18
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-08-18 11:08:11 +0000

    Added getters for all variables and tests for them

--- code.js
+++ code.js
@@ -11,7 +11,61 @@ function Book(title, author, pages, hasBeenRead) {
   this.hasBeenRead = hasBeenRead;
 }
 
+Book.prototype.getTitle = function() {
+    return this.title;
+}
+
+Book.prototype.getAuthor = function() {
+    return this.author;
+}
+
+Book.prototype.getPages = function() {
+    return this.pages;
+}
+
+Book.prototype.getHasBeenRead = function() {
+    return this.hasBeenRead;
+}
+
 function addBookToLibrary(title, author, pages, hasBeenRead) {
     book = new Book(title, author, pages, hasBeenRead);
     myLibrary.push(book);
 }
+
+addBookToLibrary("The Way Of Kings", "Brandon Sanderson", 1271, true);
+addBookToLibrary("The Wheel of Time", "Robert Jordan", 890, true);
+addBookToLibrary("The Strength of The Few", "James Islington", 980, false);
+
+myLibrary.forEach((book) => {
+    console.log(book.getTitle());
+    console.log(book.getAuthor());
+    console.log(book.getPages());
+    console.log(book.getHasBeenRead());
+})
+
+// async function searchBook(book) {
+//     const title = document.getElementById('bookTitle').value;
+//     const url = `https://openlibrary.org/search.json?title=${encodeURIComponent(title)}`;
+
+//     const response = await fetch(url);
+//     const data = await response.json();
+
+//     // Check if any results
+//     if (data.docs.length > 0) {
+//     const firstResult = data.docs[0];
+
+//     if (firstResult.cover_i) {
+//         const coverId = firstResult.cover_i;
+//         const coverUrl = `https://covers.openlibrary.org/b/id/${coverId}-L.jpg`;
+
+//         document.getElementById('coverResult').innerHTML = `
+//         <p><strong>${firstResult.title}</strong> by ${firstResult.author_name?.[0] || 'Unknown Author'}</p>
+//         <img src="${coverUrl}" alt="Book Cover">
+//         `;
+//     } else {
+//         document.getElementById('coverResult').innerText = 'No cover image found.';
+//     }
+//     } else {
+//         document.getElementById('coverResult').innerText = 'No results found.';
+//     }
+// }
